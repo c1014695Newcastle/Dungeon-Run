57ab0987cf754df12c19e1189152788823539632
c1014695Newcastle <c1014695@newcastle.ac.uk>
2022-06-29 00:16:55 +0100

    Added the method to start a room encounter and direct the user to make a decision on which attack they use

--- Room.java
+++ Room.java
@@ -39,6 +39,13 @@ public class Room {
     }
 
 
+    /**
+     * Generates the enemies for the room encounter based on the set difficulty of the room, the number of enemies and the type of encounter this is
+     * @param numOfEnemies the number of enemies which should spawn in this encounter
+     * @param type the type of encounter, whether it be flame, poison or necrotic
+     * @param difficulty the difficulty of this room as a set number, 1 - easy, 2 - med, 3 - hard
+     * @return the list of enemies for this encounter.
+     */
     private ArrayList<Enemy> generateEnemies(int numOfEnemies, String type, int difficulty) {
         Random rn = new Random();
         ArrayList<Enemy> enemies = new ArrayList<>();
@@ -47,13 +54,16 @@ public class Room {
                 String name = Enemy.fireTypes[rn.nextInt(Enemy.fireTypes.length)];
                 int level = rn.nextInt(5) + 1;
                 if (name.equals("Draugr")) {
-                    Enemy enemy = new Enemy(x + 1, level, level * 10, level * 10, level * 5, name);
+                    String[] attacks = {"Chop", "Swing", "Bash"};
+                    Enemy enemy = new Enemy(x + 1, level, 15 + level * 10, 15 +level * 10, level * 5, name, attacks);
                     enemies.add(enemy);
                 } else if (name.equals("Fire Demon")){
-                    Enemy enemy = new Enemy(x + 1, level, level * 5, level * 5, level * 10, name);
+                    String[] attacks = {"Spit", "Bite", "Blind"};
+                    Enemy enemy = new Enemy(x + 1, level, 15 + level * 5, 15 + level * 5, level * 10, name, attacks);
                     enemies.add(enemy);
                 } else {
-                    Enemy enemy = new Enemy(x + 1, level, level * 10, level * 10, level * 10, name);
+                    String[] attacks = {"Smoke", "Crush", "Burn"};
+                    Enemy enemy = new Enemy(x + 1, level, 15 + level * 10, 15 + level * 10, level * 10, name, attacks);
                     enemies.add(enemy);
                 }
             }
@@ -61,10 +71,30 @@ public class Room {
         return enemies;
     }
 
-    public static void main(String[] args) {
-        Room roomOne = new Room(1, "Flame");
-        for (Enemy e : roomOne.getEnemies()){
-            System.out.println(e.getName());
+    public void startEncounter(Player p){
+        String choice;
+        int damage;
+        while (!getEnemies().isEmpty()){
+            for (Enemy e : getEnemies()){
+                System.out.println(e);
+            }
+            choice = GameIO.playerChoice();
+            if (choice.equals("1")){
+                //damage = p.cast();
+            } else if (choice.equals("2")){
+                damage = p.chop();
+
+            } else {
+                damage = p.swing();
+            }
+
         }
+        //endEncounter(p);
     }
+
+    private void playerTurn(){
+
+    }
+
+
 }
