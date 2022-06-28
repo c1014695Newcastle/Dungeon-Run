9d0c950867c4478d7960d7c897a3a5c71d4723e6
c1014695Newcastle <c1014695@newcastle.ac.uk>
2022-06-29 00:13:01 +0100

    Added methods to allow the user to choose their attack against an enemy, methods to validate this input and return it to the calling method.

--- GameIO.java
+++ GameIO.java
@@ -4,18 +4,42 @@ import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class GameIO {
+
+    public static String playerChoice(){
+        boolean check = true;
+        System.out.print("""
+                Pick an attack:
+                    1 - Cast
+                    2 - Chop
+                    3 - Swing
+                    
+                > """);
+        Scanner s = new Scanner(System.in);
+        String choice = "";
+        while (check){
+            System.out.print("Postcode: ");
+            choice = s.nextLine();
+            check = checkChoice(choice);
+            if (check) {
+                System.out.println("INVALID - TRY AGAIN");
+            } else {
+                System.out.println("Accepted");
+            }
+        }
+        return choice;
+    }
+
+    private static boolean checkChoice(String choice){
+        return (choice.matches(".*[a-zA-Z!@#$%^&*)(+=._-]+.*") | choice.isEmpty()); // (5)
+    }
+
     public static int playerRoll(boolean poisoned, boolean necrosis, boolean burned ){
-        System.out.println("Press enter to roll the dice");
+        System.out.print("Press enter to roll the dice");
         Scanner scanner = new Scanner(System.in);
         scanner.nextLine();
         Random rn = new Random();
         int rollOne = rn.nextInt(10) + 1;
         int rollTwo = rn.nextInt(10) + 1;
-        try {
-            TimeUnit.SECONDS.sleep(1);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
 
         System.out.format("""
                 
