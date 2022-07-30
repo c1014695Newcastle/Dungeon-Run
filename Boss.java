8af355108ee44b40310fe190cdfd8db5c31bd347
c1014695Newcastle <c1014695@newcastle.ac.uk>
2022-07-30 21:02:40 +0100

    Added an Enum for the boss names and replaced all string name switch statements with enum values, added Fenrir's attacks

--- Boss.java
+++ Boss.java
@@ -8,16 +8,20 @@ import java.util.Random;
  * faf - Fafnir
  * fen - Fenrir
  * jor - Jormungandr
+ * lok - Loki
  */
 
 public class Boss {
+
+    enum bossName {
+        FAFNIR, JORMUNGANDR, FENRIR, LOKI
+    }
     private final int level;
     private int possibleHealth;
     private int health;
     private int baseDamage;
-    private final String name;
+    private final bossName name;
     private final int xp;
-    private ArrayList<Enemy> enemies;
     private int phase;
     private final String[] lines;
 
@@ -49,20 +53,12 @@ public class Boss {
         this.baseDamage = baseDamage;
     }
 
-    public String getName() {
+    public bossName getName() {
         return name;
     }
 
     public int getXp() {
         return xp;
-    }
-
-    public ArrayList<Enemy> getEnemies() {
-        return enemies;
-    }
-
-    public void setEnemies(ArrayList<Enemy> enemies) {
-        this.enemies = enemies;
     }
 
     public int getPhase() {
@@ -77,42 +73,40 @@ public class Boss {
         return lines;
     }
 
-    public Boss(int level, String name) {
+    public Boss(int level, bossName name) {
         this.level = level;
         this.possibleHealth = 200 + (level * 50);
         this.health = possibleHealth;
         this.baseDamage = level * 20;
         this.name = name;
         this.xp = level * 50;
-        this.enemies = null;
         this.phase = 1;
         this.lines = setLines(name);
     }
 
-    private String[] setLines(String name) {
+    private String[] setLines(bossName name) {
         switch (name){
-            case "Fafnir" -> {
+            case FAFNIR-> {
                 return new String[]{"You shall not escape.", "You dare challenge me.", "You shall burn.", "None shall take my treasure", "He will have you."};
             }
-            case "Fenrir" -> {
-                return null;
+            case FENRIR -> {
+                return new String[]{"You shall not escape.", "You dare challenge me.", "You shall burn.", "None shall take my treasure", "He will have you."};
             }
-            case "Jormungandr" -> {
-                return null;
+            case JORMUNGANDR -> {
+                return new String[]{"You shall not escape.", "You dare challenge me.", "You shall burn.", "None shall take my treasure", "He will have you."};
             }
         }
         return null;
     }
 
-
     protected int fafPhaseOneAttack(Player p){
         Random rn = new Random();
         int roll = rn.nextInt(2) + 1;
         if (roll == 1){
-            GameIO.reportAttack(name, "Bite");
+            GameIO.reportAttack(name.toString(), "Bite");
             return standardDamage(rn);
         } else {
-            GameIO.reportAttack( name, "Fire");
+            GameIO.reportAttack( name.toString(), "Fire");
             return fireDamage(p, rn);
         }
     }
@@ -121,13 +115,13 @@ public class Boss {
         Random rn = new Random();
         int roll = rn.nextInt(3) + 1;
         if (roll == 1){
-            GameIO.reportAttack(name, "Bite");
+            GameIO.reportAttack(name.toString(), "Bite");
             return standardDamage(rn);
         } else if (roll == 2) {
-            GameIO.reportAttack( name, "Fire");
+            GameIO.reportAttack( name.toString(), "Fire");
             return fireDamage(p, rn);
         } else {
-            GameIO.reportAttack( name, "Dive");
+            GameIO.reportAttack( name.toString(), "Dive");
             return standardDamage(rn) + 10;
         }
     }
@@ -136,21 +130,80 @@ public class Boss {
         Random rn = new Random();
         int roll = rn.nextInt(4) + 1;
         if (roll == 1){
-            GameIO.reportAttack(name, "Bite");
+            GameIO.reportAttack(name.toString(), "Bite");
             return standardDamage(rn);
         } else if (roll == 2) {
-            GameIO.reportAttack( name, "Fire");
+            GameIO.reportAttack( name.toString(), "Fire");
             return fireDamage(p, rn);
         } else if (roll == 3){
-            GameIO.reportAttack( name, "Dive");
+            GameIO.reportAttack( name.toString(), "Dive");
             return standardDamage(rn) + 10;
         } else {
-            GameIO.reportAttack(name, "Talon Swipe");
+            GameIO.reportAttack(name.toString(), "Talon Swipe");
             return  fireDamage(p, rn) + 10;
         }
     }
 
 
+    protected int fenPhaseOneAttack(Player p){
+        Random rn = new Random();
+        int roll = rn.nextInt(2) + 1;
+        if (roll == 1){
+            GameIO.reportAttack(name.toString(), "Bite");
+            return standardDamage(rn);
+        } else {
+            GameIO.reportAttack( name.toString(), "Fire");
+            return necroticDamage(p, rn);
+        }
+    }
+
+    protected int fenPhaseTwoAttack(Player p){
+        Random rn = new Random();
+        int roll = rn.nextInt(3) + 1;
+        if (roll == 1){
+            GameIO.reportAttack(name.toString(), "Bite");
+            return standardDamage(rn);
+        } else if (roll == 2) {
+            GameIO.reportAttack( name.toString(), "Fire");
+            return necroticDamage(p, rn);
+        } else {
+            GameIO.reportAttack( name.toString(), "Dive");
+            return standardDamage(rn) + 10;
+        }
+    }
+
+    protected int fenPhaseThreeAttack(Player p){
+        Random rn = new Random();
+        int roll = rn.nextInt(4) + 1;
+        if (roll == 1){
+            GameIO.reportAttack(name.toString(), "Bite");
+            return standardDamage(rn);
+        } else if (roll == 2) {
+            GameIO.reportAttack( name.toString(), "Fire");
+            return necroticDamage(p, rn);
+        } else if (roll == 3){
+            GameIO.reportAttack( name.toString(), "Dive");
+            return standardDamage(rn) + 10;
+        } else {
+            GameIO.reportAttack(name.toString(), "Talon Swipe");
+            return  necroticDamage(p, rn) + 10;
+        }
+    }
+
+    private int necroticDamage(Player p, Random rn){
+        int roll = rn.nextInt(6) + 1;
+        if (roll < 2){
+            return 0;
+        } else if (roll < 4){
+            p.setNecrosis(true);
+            p.setDebuffCounter(3);
+            return baseDamage;
+        } else {
+            p.setNecrosis(true);
+            p.setDebuffCounter(5);
+            return baseDamage * 2;
+        }
+    }
 
 
     private int standardDamage(Random rn){
@@ -180,7 +233,7 @@ public class Boss {
     }
 
     public static void main(String[] args) {
-        Boss d = new Boss(1, "Fafnir");
+        Boss d = new Boss(1, bossName.FAFNIR);
         Player p = new Player("Luke");
         BossRoom b = new BossRoom(1, Difficulty.LOW, Types.FIRE, d);
         b.bossEncounter(p);
@@ -192,7 +245,7 @@ public class Boss {
         String topBar = GameIO.topBorder(healthBar.length() + 12);
         String bottomBar = GameIO.bottomBorder(healthBar.length() + 12);
         return topBar +
-                "\t" + name.replace("_"," ") + "\n"  + "\tHealth: " + healthBar + "\n    "+ health + "\\" + possibleHealth + "\n"  +bottomBar;
+                "\t" + name.toString().replace("_"," ") + "\n"  + "\tHealth: " + healthBar + "\n    "+ health + "\\" + possibleHealth + "\n"  +bottomBar;
     }
 
 }
